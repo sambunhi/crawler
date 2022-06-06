7f9edfa49f9578a9554ec1ae11ac31145ee0973d
Leo <leo881003@gmail.com>
2022-06-06 16:16:54 +0800

    Matrix packages CI

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -5,29 +5,29 @@ parent = os.path.dirname(current)
 sys.path.append(parent)
 
 
-import crawler_sample
+from sample_crawler import GoogleCrawler
 
 def test_google_search():
-    crawler = crawler_sample.GoogleCrawler()
+    crawler = GoogleCrawler()
     query = "TSMC Ingas"
     results = crawler.google_search(query)
     assert len(results) > 0
 
 def test_get_source():
-    crawler = crawler_sample.GoogleCrawler()
+    crawler = GoogleCrawler()
     target_url = 'https://www.reuters.com/technology/exclusive-ukraine-halts-half-worlds-neon-output-chips-clouding-outlook-2022-03-11/'
     response = crawler.get_source(target_url)
     assert response.status_code == 200
 
 def test_scrape_google():
     query = 'https://www.google.com/search?q='+"TSMC Ingas"
-    crawler = crawler_sample.GoogleCrawler()
+    crawler = GoogleCrawler()
     results = crawler.scrape_google(query)
     assert len(results) > 0
 
 
 def test_html_getText():
-    crawler = crawler_sample.GoogleCrawler()
+    crawler = GoogleCrawler()
     target_url = 'https://www.reuters.com/technology/exclusive-ukraine-halts-half-worlds-neon-output-chips-clouding-outlook-2022-03-11/'
     response = crawler.get_source(target_url)
     orignal_text = crawler.html_getText(response.text)
