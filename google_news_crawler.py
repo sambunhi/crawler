31d729909ebc841498489ac623f8b74726fabf05
Leo <leo881003@gmail.com>
2022-06-07 17:50:27 +0800

    Connect google_news_crawler with API

--- google_news_crawler.py
+++ google_news_crawler.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import json
+from urllib.parse import urljoin
+import os
+from sys import stderr
 import requests
 from requests_html import HTML, HTMLSession
 from bs4 import BeautifulSoup
@@ -22,15 +26,15 @@ class GoogleNewsCrawler:
     # Google Search Result Parsing
     def parse_google_results(self, response):
         soup = BeautifulSoup(response.text, 'html.parser')
-        results = soup.findAll("g-card")
+        rso = soup.find(id="rso")
+        results = rso.findAll("a")
         output = []
-        for card in results:
-            a = card.find("a")
+        for a in results:
             title = a.find("div", {"role": "heading"})
             item = {
                 'title': title.get_text(),
-                'link': a['href'],
-                'text': title.find_next_sibling("div").get_text()
+                'url': a['href'],
+                #'text': title.find_next_sibling("div").get_text()
             }
             output.append(item)
         return output
@@ -47,11 +51,39 @@ class GoogleNewsCrawler:
             num=num,
             tbm=timeline,
         )
-        return self.parse_google_results(response)
+        #return self.parse_google_results(response)
+        r = self.parse_google_results(response)
+        if len(r) == 0:
+            print(response.text)
+        return r
 
 if __name__ == "__main__":
-    query = "TSMC ASML"
+    api_baseurl = os.getenv("CRAWLER_API_URL", "https://sambunhi.nycu.one")
+    dry_run = "CRAWLER_DRYRUN" in os.environ
+
+    response = requests.get(urljoin(api_baseurl, "/api/v1/crawler"))
+    response.raise_for_status()
+    keywords = response.json()["keywords"]
 
     crawler = GoogleNewsCrawler()
-    results = crawler.google_search(query, num=10, timeline='qdr:d')
-    print(results)
+    for k in keywords:
+        print(f"Searching: {k}", file=stderr)
+        try:
+            results = crawler.google_search(k, num=50, timeline='qdr:d')
+            print(f"Found {len(results)} results", file=stderr)
+        except Exception as e:
+            print(f"Error occurred while searching: {e}", file=stderr)
+            continue
+
+        if not dry_run:
+            try:
+                r = requests.put(urljoin(api_baseurl, "/api/v1/article"), json=results)
+                r.raise_for_status()
+                print("Upload completed!", file=stderr)
+            except requests.exceptions.RequestException as e:
+                print(f"Error occurred while uploading: {e}", file=stderr)
+        else:
+            print("Running in dryrun mode. Skip uploading!", file=stderr)
+            print(json.dumps(results, indent=2, ensure_ascii=False))
+
+    print("Google News Crawler completed!", file=stderr)
